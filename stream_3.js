02c646b480db1b906c862a24453ffe07fd0ee03b
hochitom <hochitom@icloud.com>
2013-06-24 08:29:45 +0200

    created writable stream

--- stream_3.js
+++ stream_3.js
@@ -1,1 +1,23 @@
 var Stream = require('stream');
+var s = new Stream;
+s.writeable = true;
+
+var bytes = 0;
+
+s.write = function (buf) {
+    bytes += buf.length;
+}
+
+s.end = function (buf) {
+    if (arguments.length) s.write(buf);
+
+    s.writeable = false;
+    console.log(bytes, 'bytes written');
+}
+
+s.destroy = function () {
+    s.writeable = false;
+}
+
+var fs = require('fs');
+fs.createReadStream('data.txt').pipe(s);
